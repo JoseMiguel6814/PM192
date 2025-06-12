a44ed669d2ce05c60880dacb92e05e378917d527
Perusquia Hernandez Jose Miguel <jowsjose6814@gmail.com>
2025-06-11 18:19:21 -0600

    componentes

--- my-blank/App.js
+++ my-blank/App.js
@@ -1,15 +1,43 @@
+/*Zona 1 Importaciones */
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-web';
 
+const Texto = (props) => {
+  const { contenido } = props
+  return (
+    <Text> {contenido} </Text>
+  );
+}
+
+
+
+
+/*Zona 2 Main  */
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style='auto' />
+
+   
+      <Texto contenido="hola"></Texto>
+      <Texto contenido="mundo"></Texto>
+      <Texto contenido="react native"></Texto>
+
+
+      <Button title='presioname'></Button>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+
+
+
+
+
+
+/*Zona 3 estilos */
 const styles = StyleSheet.create({
   container: {
     flex: 1,
