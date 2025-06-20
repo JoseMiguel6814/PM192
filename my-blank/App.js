3cd40df49888bdd531d36234ca2b5b9a6481314d
Perusquia Hernandez Jose Miguel <jowsjose6814@gmail.com>
2025-06-19 18:19:46 -0600

    Text Input

--- my-blank/App.js
+++ my-blank/App.js
@@ -1,42 +1,41 @@
 /*Zona 1 Importaciones */
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Button } from 'react-native-web';
-import React, { useState } from 'react';
+import React,{useState} from "react";
 
-const Texto = ({style}) => {
-  const [contenido, setContenido] = useState('Hola Mundo');
-  const actualizarTexto = () => {
-    setContenido('Estado actualizado');
-  }
-
-
-  return (
-    <Text style={[styles.text, style]} onPress={actualizarTexto}>{contenido}</Text>
-  );
-}
+import { View,Text, TextInput, Button, Alert, StyleSheet } from "react-native-web";
 
 
 
 
 /*Zona 2 Main  */
 export default function App() {
+  const [nombre, setNombre] = useState('');
+  
+  const mostrarAlerta =()=>{
+    if (nombre.trim()=== ''){
+      Alert.alert('error', 'porfavor escribe algo');
+      alert('Escribe algo');
+    }else{
+      Alert.alert('bienvenido', `hola ${nombre}, bienvenido a nuestra app :p`);
+      alert(`Hola ${nombre}, bienvenido`);
+    }
+  }
   return (
     <View style={styles.container}>
-      <StatusBar style='auto' />
+      <Text style={styles.text}>Escribe tu nombre:</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Escribe tu nombre"
+        value={nombre}
+        onChangeText={setNombre}
+      >
+      </TextInput>
 
-   
-      <Texto style={styles.red}></Texto>
-      <Texto style={styles.green}></Texto>
-      <Texto style={styles.blue}></Texto>
-    
-
-
-      <Button title='presioname'></Button>
-      <StatusBar style="auto" />
+      <Button title="Enviar" onPress={mostrarAlerta}></Button>
     </View>
-  );
-}
+  )
+ 
+};
+
 
 
 
@@ -48,18 +47,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'stretch',
+    backgroundColor: '#f5f5f5',
+    pudding: 20,
     justifyContent: 'center',
-    flexDirection: 'column',
   },
   text: {
-    fontSize: 20,
-    color: 'white',
-    height:100,
-    
+    fontSize: 18,
+    marginBottom: 10,
+    color: 'black',
   },
-  red:{backgroundColor: 'red'},
-  green:{backgroundColor: 'green'},
-  blue:{backgroundColor: 'blue'},
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    padding: 10,
+    borderRadius: 5,
+    backgroundColor: '#f0f0f0',
+    color: 'black',
+  },
+ 
 });
